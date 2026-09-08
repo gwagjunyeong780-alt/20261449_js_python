e6f0131016c6f57657ae05605be9705727c037df
junyeongkwak <gwagjunyeong780@gmail.com>
2026-09-08 15:59:13 +0900

    하위 폴더 및 파일 추가

---
+++ python/2주차.py
@@ -0,0 +1,20 @@
+print("hello, world")
+print(10 + 10)
+print(20 * 5)
+print(10/2)
+print(5-2)
+print("30*30=" , 30*30)
+print("123*456*789=",123*456*789)
+print("100/200/300=",100/200/300)
+a = str(input("첫번째 값 :" ))  
+b = str(input("두번째 값 :"))
+## result = a*b 
+## result1 = a/b 
+## result2 = a+b
+## result3 = a-b
+## print(result)
+## print(a, "*", b, "=", result)
+## print(a,"/",b,"=",result1)
+## print(a,"+",b,"=",result2)
+## print(a,"-",b,"=",result3)
+print(a,"and",b)
